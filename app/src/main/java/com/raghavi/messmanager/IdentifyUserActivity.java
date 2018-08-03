845500963bad0707e59080173b5b2a33d8f673bf
Raghavi <raghavi.adoni@cumminscollege.in>
2018-08-03 19:19:54 +0530

    To do: payment integration

--- app/src/main/java/com/raghavi/messmanager/IdentifyUserActivity.java
+++ app/src/main/java/com/raghavi/messmanager/IdentifyUserActivity.java
@@ -3,6 +3,7 @@ package com.raghavi.messmanager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +11,12 @@ import android.view.View;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import static com.raghavi.messmanager.MainActivity.mFirebaseDatabase;
 
@@ -25,52 +30,98 @@ public class IdentifyUserActivity extends AppCompatActivity {
     private DatabaseReference mUsersDatabaseReference;
     private DatabaseReference mEmployeeDatabaseReference;
     public static String userEmailID;
+    DatabaseReference ref;
+    int flag;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify_user);
-        sharedPreferences=getApplicationContext().getSharedPreferences("com.raghavi.messmanager", Context.MODE_PRIVATE);
+        sharedPreferences = getApplicationContext().getSharedPreferences("com.raghavi.messmanager", Context.MODE_PRIVATE);
 
+        flag=0;
         firebaseDatabase = FirebaseDatabase.getInstance();
-       mUsersDatabaseReference = firebaseDatabase.getReference().child("students");
-        mEmployeeDatabaseReference= firebaseDatabase.getReference().child("employee");
+        mUsersDatabaseReference = firebaseDatabase.getReference().child("students");
+        mEmployeeDatabaseReference = firebaseDatabase.getReference().child("employee");
 
-        userEmailID=sharedPreferences.getString("User_Email_id", "Unidentified");
+        userEmailID = sharedPreferences.getString("User_Email_id", "Unidentified");
 //To:do
-/*        if(!userEmailID.endsWith("@cumminscollege.in"))
-        {
-            Toast.makeText(this,"Please use cummins college email id",Toast.LENGTH_SHORT).show();
-            finish();
-        }*/
+
     }
 
-   public void onMessPersonButtonClick(View view)
-    {
+    public void onMessPersonButtonClick(View view) {
         //mUsersDatabaseReference.push().setValue(sharedPreferences.getString("User_Email_id", "Unidentified"));
 
-        mEmployeeDatabaseReference.push().setValue(userEmailID);
 
-        Intent i = new Intent(this, PasswordActivity.class);
-        sharedPreferences.edit().putString("UserType","Employee").apply();
-        startActivity(i);
-
+        if (!userEmailID.endsWith("@cumminscollege.in")) {
+            mEmployeeDatabaseReference.push().setValue(userEmailID);
+            Intent i = new Intent(this, PasswordActivity.class);
+            sharedPreferences.edit().putString("UserType", "Employee").apply();
+            startActivity(i);
+        } else {
+            Toast.makeText(this, "Please use your proper email id", Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
     }
 
     public void onStudentRadioButtonClick(View view) {
 
 
-        mUsersDatabaseReference.push().setValue(userEmailID);
-        Intent i = new Intent(this, MainActivity.class);
-        sharedPreferences.edit().putString("UserType","Student").apply();
-        startActivity(i);
-        finish();
+            if(userEmailID.endsWith("@cumminscollege.in")){
+            ref = FirebaseDatabase.getInstance().getReference();
+            Query foodItemQuery = ref.child("studentsData").orderByChild("email").equalTo(userEmailID);
 
-       // userEmailID=sharedPreferences.getString("User_Email_id", "Unidentified");
+            foodItemQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        flag=1;
+                        mUsersDatabaseReference.push().setValue(userEmailID);
+                        Intent i = new Intent(getApplication(), MainActivity.class);
+                        sharedPreferences.edit().putString("UserType", "Student").apply();
+                        startActivity(i);
+                        finish();
+                    }
+                    if(flag!=1)
+                    {
+                        Toast.makeText(getApplicationContext(), "Invalid id or unregistered user", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+            /*if(!userEmailID.endsWith("@cumminscollege.in"))
+            {
+                Toast.makeText(this, "Please use cummins college email id", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+           /* if(flag==1) {
+                mUsersDatabaseReference.push().setValue(userEmailID);
+                Intent i = new Intent(this, MainActivity.class);
+                sharedPreferences.edit().putString("UserType", "Student").apply();
+                startActivity(i);
+                finish();
+            }
+            else
+           if(flag!=1)
+            {
+                Toast.makeText(this, "Access allowed to only hostel students", Toast.LENGTH_SHORT).show();
+                finish();
+            }*/
     }
-    public void onClick(View view)
-    {
-        startActivity(new Intent(this,MainActivity.class));
+
+
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
